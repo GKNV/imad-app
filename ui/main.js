e690f7b61243bc04de919bfaf1964fdf90e8b4d8
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-11 20:06:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,13 @@ img.onclick =function(){
     
    var intervel =setInterval(moveRight,50);
 };
+
+
+
+var button= document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    counter++;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
