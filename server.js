bb7e637243f99e9cbfffa43458c93d261df001b8
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-11 12:45:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'mail.js'));
 });
