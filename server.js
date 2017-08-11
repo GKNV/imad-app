a6201b11a72f77bd19d259c6951807ea614683df
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-11 11:14:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var Articleone ={
+    title: 'gopi1',
+    heading:'Article-one',
+    date:'Aug 10,2017',
+    content:`
+<p>Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works</p>
+<p>Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works</p>
+<p>Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works Hi , This is first test html page. To check how html works</p>    
+    `
+    
+};
+
+
+function createTemplate (data){
+    
+    var title= data.title;
+    var date= data.date;
+    var heading= data.heading;
+    var content= data.content;
+    
+    
+
+
+var htmlTemplate=`
+
+<html>
+<head><title>
+${title}
+</title>
+<meta name ="viewport" content="width-device-width, initial-scale=1">
+<link href="/ui/style.css" rel="stylesheet" />
+
+</head>
+<body><div class="container">
+<div>
+<a href='/'>home</a>
+
+</div>
+<hr/>
+<h3>
+${heading}
+</h3>
+<div>
+${date}
+</div>
+<div>
+${content}
+</div>
+</div>
+</body>
+</html>
+
+
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +72,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/gopi1.html', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'gopi1.html'));
+    res.send(createTemplate(Articleone));
 });
 
 
