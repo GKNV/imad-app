6754b57f3a0e349b6971847093cc1a5636d02841
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-11 21:55:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,18 +19,18 @@
 var button= document.getElementById('counter');
 button.onclick = function(){
 
-var request = new XMLHttpRequest();
+var httpRequest = new XMLHttpRequest();
 
-request.onreadystatechange= function(){
-    if(request.readyState === XMLHttpRequest.Done){
-        if(request.status === 200){
+httpRequest.onreadystatechange= function(){
+    if(httpRequest.readyState === XMLHttpRequest.Done){
+        if(httpRequest.status === 200){
             var counter = request.responseText;
             var span = document.getElementById('count');
             span.innerHTML= counter.toString();
         }
     }
 };
-request.open('GET','http://gopikanna09.imad.hasura-app.io/counter',true);
-reqest.send(null);
 
+httpRequest.open('GET', 'http://gopikanna09.imad.hasura-app.io/counter', true);
+httpRequest.send();
 };
