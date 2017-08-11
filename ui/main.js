5c10d2389ce7681571e70668a85ddba0406682b2
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-11 23:17:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,33 +1,19 @@
-
-var button= document.getElementById('counter');
-button.onclick = function(){
-
-var request = new XMLHttpRequest();
-
-request.onreadystatechange= function(){
-    if(request.readyState === XMLHttpRequest.Done){
-        if(request.status === 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML= counter.toString();
-        }
-    }
-};
-
-request.open("GET", "http://gopikanna09.imad.hasura-app.io/counter", true);
-request.send(null);
-};
-
-
-var nameInput=document.getElementById('name');
-var name = nameInput.value;
-var submit= document.getElementById('submit_btn');
-submit.onclick = function(){
-    var names= ['name1','name2','name3','name4'];
-    var list = '';
-for (var i=0; i< names.length,i++){
-    list += '<li>' + names[i] + '<li>';
+function exec(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() 
+    {
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    var counter = request.responseText; 
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                } 
+          } 
+    }; 
+    request.open("GET", "http://gopikanna09.imad.hasura-app.io/counter", true);
+    request.send(); 
+    console.log('EXECUTED');
 }
- var ul= document.getElementById('namelist');   
-    ul.innerHTML = list;
-};
