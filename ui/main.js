e08c66900983eb4ae41550bc419ee5e116ca94bc
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-11 20:09:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,24 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML='so tired of doing things';
+//console.log('Loaded!');
+//var element = document.getElementById('main-text');
+//element.innerHTML='so tired of doing things';
 
-var img = document.getElementById('madi');
-var marginLeft =0;
-function moveRight() {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft= marginLeft + 'px';
+//var img = document.getElementById('madi');
+//var marginLeft =0;
+//function moveRight() {
+  //  marginLeft = marginLeft + 5;
+    //img.style.marginLeft= marginLeft + 'px';
     
-}
-img.onclick =function(){
+//}
+//img.onclick =function(){
     
-   var intervel =setInterval(moveRight,50);
-};
+  // var intervel =setInterval(moveRight,50);
+//};
 
 
 
 var button= document.getElementById('counter');
 var counter = 0;
+
 button.onclick = function(){
     counter=counter+1;
     var span = document.getElementById('count');
