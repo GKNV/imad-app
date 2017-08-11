ab1f53d70ac03a84109eaab98885f7305266acda
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-11 12:25:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/:articlesName', function(req,res){
+app.get('/:articleName', function(req,res){
     var articleName= req.params.articleName;
     res.send(createTemplate(articles[articlesName]));
 });
