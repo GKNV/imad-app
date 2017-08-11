ee2e2f9fb990760524db9406e916dcf317bf64f9
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-11 13:52:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='so tired of doing things';
 
-var img= document.getElementById('mad');
+var img= document.getElementById('madi');
 img.onclick =function(){
     
     img.style.marginleft= '100px';
