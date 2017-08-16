46756c571e05ea509bf194ef1460cf37864eed0e
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-17 03:15:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,16 @@ app.get('/counter', function (req, res) {
       res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name/', function(req,res) {
+    
+    var name = req.query.name;
+    
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -123,14 +133,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-var names = [];
-app.get('/submit-name/:name', function(req,res) {
-    
-    var name = req.params.name;
-    
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
