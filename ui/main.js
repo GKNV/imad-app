4742f4071de1ae07516aad19ba7156ad2947c861
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-17 03:41:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,15 +20,34 @@ function exec(){
 
 
 
-var nameInput=document.getElementById('name');
-var name = nameInput.value;
+
 var submit= document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names= ['name1','name2','name3','name4'];
+    
+     var request = new XMLHttpRequest();
+    request.onreadystatechange = function() 
+    {
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                   
+    var names= request.responsetest;
+    names= JSON.parse(names);
     var list = '';
 for(var i=0; i< names.length;i++){
     list += '<li>' + names[i] + '</li>';
 }
  var ul= document.getElementById('namelist');   
     ul.innerHTML = list;
+                }
+          }
+};
+
+var nameInput=document.getElementById('name');
+var name = nameInput.value;
+    request.open("GET", "http://gopikanna09.imad.hasura-app.io/submit-name?name=" +name, true);
+    request.send(); 
+    console.log('EXECUTED');
 };
