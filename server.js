2540691abf1ab0019e386aaf62aaf088df3a125f
GKNV <GOPIKANNA09@GMAIL.COM>
2017-08-28 11:19:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var config={
     
     user:'gopikanna09',
     database:'gopikanna09',
-    host:'db.imad.hasura.-app.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:'process.env.DB_PASSWORD'
 };
